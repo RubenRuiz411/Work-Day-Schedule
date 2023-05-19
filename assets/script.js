400ff63b497d23d5adafee50b1101e2273539557
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-05-18 23:23:35 -0700

    added comments to code

--- assets/script.js
+++ assets/script.js
@@ -7,21 +7,23 @@
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // useful when saving the description in local storage? **
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // current hour in 24-hour time? **
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // attribute of each time-block be used to do this? **
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // TODO: Add code to display the current date in the header of the page. **
 
 
+
+// this adds functionality to any of the save buttons when they are  clicked, saves the hour and text value to local storage
 $(function () {
   $(".saveBtn").click(function(){
     var value = $(this).siblings(".description").val()
@@ -29,8 +31,9 @@ $(function () {
     localStorage.setItem(time,value) 
 
   })
-
-  $("#currentDay").text(dayjs().format("dddd, MMMM YYYY"));
+  // displaying current date on header of page 
+  $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY"));
+  // function that checks time and compares to timeblocks on html and udpates the CSS accordingly (changes color if hour is in past or in future of current time)
   $(".time-block").each(function(){
     var currenthour = dayjs().hour()
     var timeblock = parseInt($(this).attr("id").split("-")[1])
@@ -43,7 +46,7 @@ $(function () {
     } 
 
   })
-
+  //retreiving saved value from local storage for each hour upon launch
   $("#hour-9 .description").val(localStorage.getItem("hour-9"))
   $("#hour-10 .description").val(localStorage.getItem("hour-10"))
   $("#hour-11 .description").val(localStorage.getItem("hour-11"))
